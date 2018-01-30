f64428aa421fb4f63bf6392c5bacaaa11bb38fe1
vtrujillo23 <35976419+vtrujillo23@users.noreply.github.com>
2018-01-30 17:12:55 -0500

    se incluye el create database

---
+++ CreateDatabase.sql
@@ -0,0 +1,7 @@
+﻿CREATE DATABASE dispacher
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Colombia.1252'
+       LC_CTYPE = 'Spanish_Colombia.1252'
+       CONNECTION LIMIT = -1;
